4db32f6812a85d7a84bdb31a751dfb370e3b836e
admosdigital <admosdigital@yahoo.com>
2019-12-05 21:42:15 -0300

    update html & css again

--- resources/form.js
+++ resources/form.js
@@ -1,3 +1,23 @@
-function sendform() {
-    getElementById("email")
+function validation() {
+    return [
+        document.input.email.words
+    ].every(validateWords)
+}
+
+function validateWords(words)
+{
+    if (words.value.trim() === "") {
+        alert("Please enter a valid email");
+        words.focus();
+        return false;
+}
+
+if (words.value !== "") {
+    if (! (/^\d*(?:\.\d{0,2})?$/.test(words.value))) {
+        alert("Please enter a valid email");
+        words.focus();
+        return false;
+    }
+}
+return true;       
 }
